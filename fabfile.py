86023b695898d33d909e44b4b3bd9017a3834ec6
Johan Gustafsson <johangsmail@gmail.com>
2013-03-06 20:39:55 +0100

    Added fabric file for easier deployment

---
+++ fabfile.py
@@ -0,0 +1,22 @@
+"""
+Fabric file for deployment of new code to production server. Example usage:
+
+	fab prod deploy
+"""
+
+from fabric.api import env, run, sudo
+from fabric.context_managers import cd
+
+env.DEPLOY_DIR = 'johaneunyoung.com'
+
+
+def prod():
+	env.use_ssh_config = True
+	env.hosts = ['johaneunyoung.com']
+
+
+def deploy():
+	with cd(env.DEPLOY_DIR):
+		run('git pull')
+		run('make languages')
+		sudo('service uwsgi restart')
